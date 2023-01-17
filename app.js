4358afe99b1da8eac45182ea92b027a9dd8609c0
Jordi Grasiot <68892105+Totistar@users.noreply.github.com>
2023-01-17 18:47:56 +0100

    up

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 //Incluyo el modulo con la clase de analisis sintactico para español.
 const spanishDb = require('./modules/spanish');
 
-class span {
-    constructor(texto) {
-        let spanish = new spanishDb();
-        spanish.loadBdFromFile();
-        this.texto = sin_salto;
-        let tmp 	= spanish.analyseTextArray(texto);
-        return tmp;
+var span = {
+    constructor: texto => {
+        let spanish = new spanishDb()
+        spanish.loadBdFromFile()
+        this.texto = sin_salto
+        let tmp 	= spanish.analyseTextArray(texto)
+        return tmp
     }
 }
 
